a1693f4ee46550b5c8eb52be6415719744cfde30
Matheus Barcelos Alves <matheusbarcelosbr@gmail.com>
2025-06-10 21:09:16 -0300

    Update PrimeiroPrograma.java

--- primeiroprograma/PrimeiroPrograma.java
+++ primeiroprograma/PrimeiroPrograma.java
@@ -1,7 +1,7 @@
 package primeiroprograma;
 
 public class PrimeiroPrograma {
-    public static void main(String[] args) {
-        System.out.println("Olá, Mundo!");
-    }
+   public static void main(String[] args) {
+    System.out.println("Olá, Mundo!");
+   }
 }
